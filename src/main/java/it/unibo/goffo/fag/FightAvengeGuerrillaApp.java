85a467f37ee17d2a86eeb9f0b33e850af6eb557b
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-07-19 19:04:13 +0200

    Setup project

---
+++ src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaApp.java
@@ -0,0 +1,32 @@
+package it.unibo.goffo.fag;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+/**
+ * Main class, used to launch FXGL.
+ */
+public class FightAvengeGuerrillaApp extends GameApplication {
+
+    private static final int WIDTH_SCREEN = 800;
+    private static final int HEIGHT_SCREEN = 600;
+    private static final String APPLICATION_NAME = "Final Avenge Guerrilla";
+
+    /**
+     * Main method launch the game engine.
+     * @param args Command line arguments.
+     */
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initSettings(final GameSettings settings) {
+        settings.setWidth(WIDTH_SCREEN);
+        settings.setHeight(HEIGHT_SCREEN);
+        settings.setTitle(APPLICATION_NAME);
+    }
+}
