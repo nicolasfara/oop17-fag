dce230131bb62fb17fe3109ce40926b854e8e39d
Leonardo Marini <leonardo.marini3@studio.unibo.it>
2018-08-03 11:13:10 +0200

    typo

--- src/main/java/it/unibo/goffo/fag/entities/FagType.java
+++ src/main/java/it/unibo/goffo/fag/entities/FagType.java
@@ -1,7 +1,7 @@
 package it.unibo.goffo.fag.entities;
 
 /**
- * Enum that model all type of entities in the game.
+ * Enum that models all type of entities in the game.
  */
 public enum FagType {
     PLAYER, SIMPLE_ZOMBIE, ADVANCE_ZOMBIE;
