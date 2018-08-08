4905c992174a86376d6c7fbd081223df18fce486
Leonardo Marini <leonardo.marini3@studio.unibo.it>
2018-08-08 22:35:15 +0200

    spawn player code refactor

--- src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaApp.java
+++ src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaApp.java
@@ -1,7 +1,6 @@
 package it.unibo.goffo.fag;
 
 import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.extra.ai.pathfinding.AStarGrid;
 import com.almasb.fxgl.input.Input;
@@ -10,13 +9,8 @@ import com.almasb.fxgl.settings.GameSettings;
 import it.unibo.goffo.fag.animation.FagControl;
 import it.unibo.goffo.fag.entities.FagType;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
-import static it.unibo.goffo.fag.FagUtils.APPLICATION_NAME;
-import static it.unibo.goffo.fag.FagUtils.HEIGHT_SCREEN;
-import static it.unibo.goffo.fag.FagUtils.WIDTH_SCREEN;
-import static it.unibo.goffo.fag.FagUtils.TILE_SIZE;
+import static it.unibo.goffo.fag.FagUtils.*;
 
 /**
  * Main class, used to launch FXGL.
@@ -106,11 +100,7 @@ public class FightAvengeGuerrillaApp extends GameApplication {
         getGameWorld().addEntityFactory(new LevelFactory());
         getGameWorld().setLevelFromMap("level0.json");
 
-        player = Entities.builder()
-                .type(FagType.PLAYER)
-                .at(400,300)
-                .with(new FagControl())
-                .buildAndAttach(getGameWorld());
+        player = getGameWorld().spawn("player");
     }
 
     /**
--- src/main/java/it/unibo/goffo/fag/LevelFactory.java
+++ src/main/java/it/unibo/goffo/fag/LevelFactory.java
@@ -4,6 +4,8 @@ import com.almasb.fxgl.entity.*;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
+import it.unibo.goffo.fag.animation.FagControl;
+import it.unibo.goffo.fag.entities.FagType;
 
 //@SetEntityFactory
 public class LevelFactory implements EntityFactory {
@@ -15,6 +17,13 @@ public class LevelFactory implements EntityFactory {
                 .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
                 .with(new PhysicsComponent())
                 .build();
-
+    }
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        return Entities.builder()
+                .type(FagType.PLAYER)
+                .at(400,300)
+                .with(new FagControl())
+                .build();
     }
 }
