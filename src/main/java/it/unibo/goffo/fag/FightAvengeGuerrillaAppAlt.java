24e3a30c69b4b2ffbf3001fea7b8ebf59369a505
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-08-10 19:37:50 +0200

    collision - test with two entities

---
+++ src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaAppAlt.java
@@ -0,0 +1,123 @@
+package it.unibo.goffo.fag;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.extra.ai.pathfinding.AStarGrid;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.settings.GameSettings;
+import it.unibo.goffo.fag.animation.FagControl;
+import it.unibo.goffo.fag.entities.FagType;
+import javafx.scene.input.KeyCode;
+
+import static it.unibo.goffo.fag.FagUtils.*;
+
+/**
+ * Main class, used to launch FXGL.
+ */
+public class FightAvengeGuerrillaApp extends GameApplication {
+
+
+    private AStarGrid grid;
+    private Entity player;
+    private Entity zombie;
+
+    /**
+     * Main method launch the game engine.
+     * @param args Command line arguments.
+     */
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Return the grid used in the game.
+     * @return the game's grid.
+     */
+    public AStarGrid getGrid() {
+        return this.grid;
+    }
+
+    /**
+     * Method that return the player in the game.
+     * @return Return the player.
+     */
+    public Entity getPlayer() {
+        return getGameWorld().getSingleton(FagType.PLAYER).get();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initSettings(final GameSettings settings) {
+        settings.setWidth(WIDTH_SCREEN * TILE_SIZE);
+        settings.setHeight(HEIGHT_SCREEN * TILE_SIZE);
+        settings.setTitle(APPLICATION_NAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.getComponent(FagControl.class).moveRight();
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.getComponent(FagControl.class).moveLeft();
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.getComponent(FagControl.class).moveUp();
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.getComponent(FagControl.class).moveDown();
+            }
+        }, KeyCode.S);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initGame() {
+
+        getGameWorld().addEntityFactory(new LevelFactory());
+        getGameWorld().setLevelFromMap("level0.json");
+
+        player = getGameWorld().spawn("player");
+        zombie = getGameWorld().spawn("zombie");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initPhysics() {
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(FagType.PLAYER, FagType.SIMPLE_ZOMBIE) {
+            @Override
+            protected void onCollisionBegin(Entity collector, Entity collectible) {
+                //player.getComponent(FagControl.class).stop();
+                getDisplay().showMessageBox("collision");
+                System.out.println("collision");
+            }
+        });
+    }
+}
