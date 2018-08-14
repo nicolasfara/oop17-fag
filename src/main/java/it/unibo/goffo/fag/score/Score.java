9ca7e4a87d8324635e5f12b285b93bf61b6c285a
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-08-15 01:33:04 +0200

    update score

--- src/main/java/it/unibo/goffo/fag/score/Score.java
+++ src/main/java/it/unibo/goffo/fag/score/Score.java
@@ -1,4 +1,46 @@
 package it.unibo.goffo.fag.score;
 
-public interface Score {
+import java.time.LocalDate;
+
+/**
+ * Interface useful for create score in the game.
+ * @param <U> username's type.
+ * @param <S> score's type.
+ */
+public interface Score<U, S> {
+    /**
+     * Get the username by the score.
+     * @return the username.
+     */
+    U getUsername();
+
+    /**
+     * Set the username for the score.
+     * @param username the new username.
+     */
+    void setUsername(U username);
+
+    /**
+     * Return the score.
+     * @return the score.
+     */
+    S getScore();
+
+    /**
+     * Set the score.
+     * @param score the new score.
+     */
+    void setScore(S score);
+
+    /**
+     * Get the date of score.
+     * @return the score's date.
+     */
+    LocalDate getDate();
+
+    /**
+     * Update the new date.
+     * @param date new date.
+     */
+    void setDate(LocalDate date);
 }
