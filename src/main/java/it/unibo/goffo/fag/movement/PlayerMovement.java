a813e6a5c4f85a8b1b1439c0f3d79f8aa1c2f3f3
seintz <andrea.casadei@tutanota.com>
2018-08-21 18:38:16 +0200

    add method for movement

--- src/main/java/it/unibo/goffo/fag/movement/PlayerMovement.java
+++ src/main/java/it/unibo/goffo/fag/movement/PlayerMovement.java
@@ -1,24 +1,22 @@
 package it.unibo.goffo.fag.movement;
 
-import com.almasb.fxgl.core.logging.Logger;
-import com.almasb.fxgl.core.math.FXGLMath;
-import com.almasb.fxgl.core.math.Vec2;
-import com.almasb.fxgl.entity.components.PositionComponent;
+import com.almasb.fxgl.entity.Entity;
 
+/**
+ *
+ */
 public class PlayerMovement extends AbstractMovement {
 
 
-    private static final Logger LOG = Logger.get("FXGL");
-    private final Vec2 velocityVector = new Vec2();
-    private final PositionComponent positionComponent;
+    private final Entity entity;
 
     /**
-     * Default constructor used to assign a {@code PositionalComponent} to the movement.
-     * @param positionComponent Component used for the moving.
+     *
+     * @param entity the entity we want to move
      */
-    public PlayerMovement(final PositionComponent positionComponent) {
+    public PlayerMovement(final Entity entity) {
         super();
-        this.positionComponent = positionComponent;
+        this.entity = entity;
     }
 
     /**
@@ -26,18 +24,7 @@ public class PlayerMovement extends AbstractMovement {
      */
     @Override
     protected void move(final float deltaX, final float deltaY) {
-        if (!positionComponent.getEntity().isActive()) {
-            LOG.warning("The entity " + getEntity().toString() + "is not enable. Unable to move the entity");
-            return;
-        }
-        LOG.info("Moving the entity: " + getEntity().toString());
-        velocityVector.set(deltaX, deltaY);
-        final int vectorLength = FXGLMath.roundPositive(velocityVector.length());
-        velocityVector.normalizeLocal();
-
-        for (int i = 0; i < vectorLength; i++) {
-            positionComponent.translate(velocityVector.x, velocityVector.y);
-        }
+        entity.translate(deltaX, deltaY);
     }
 
 }
