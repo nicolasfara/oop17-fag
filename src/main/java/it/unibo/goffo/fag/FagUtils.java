aba9d7f6ac5ff2e87f323019aa1b10d5b80df816
andrcas <andrea.casadei22@studio.unibo.it>
2018-08-22 17:15:13 +0200

    add entities and utils

---
+++ src/main/java/it/unibo/goffo/fag/FagUtils.java
@@ -0,0 +1,20 @@
+package it.unibo.goffo.fag;
+
+/**
+ * Utility class.
+ */
+public final class FagUtils {
+    public static final int WIDTH_SCREEN = 800;
+    public static final int HEIGHT_SCREEN = 600;
+    public static final String APPLICATION_NAME = "Final Avenge Guerrilla";
+    public static final int BLOCK_SIZE = 10;
+    public static final int ZOMBIE_SIZE_X = 20;
+    public static final int ZOMBIE_SIZE_Y = 20;
+    public static final int PLAYER_SIZE_X = 20;
+    public static final int PLAYER_SIZE_Y = 20;
+    public static final int GRID_HEIGHT = 500;
+    public static final int GRID_WIDTH = 500;
+
+
+    private FagUtils() { }
+}
