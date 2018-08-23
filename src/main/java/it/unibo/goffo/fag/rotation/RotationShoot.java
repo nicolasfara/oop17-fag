453e82aec64c58f15b1d4e2f55e49c00c31e6131
andrcas <andrea.casadei22@studio.unibo.it>
2018-08-23 18:46:43 +0200

    add notes to rotation class

--- src/main/java/it/unibo/goffo/fag/rotation/RotationShoot.java
+++ src/main/java/it/unibo/goffo/fag/rotation/RotationShoot.java
@@ -1,45 +1,28 @@
 package it.unibo.goffo.fag.rotation;
 
-
 /*
-- create interface, abstract class and clas like Movement
 - create class for attack, maybe even interface and/or abstract
 - Into rotation Handler we call rotation and attack
-
  */
 
+/**
+ *
+ */
 public class RotationShoot extends AbstractRotation {
-/*
-        input.addAction(new UserAction("Rotate Left") {
-            @Override
-            protected void onAction() {
-                player.setRotation(-90.0);
-                onShoot();
-            }
-        }, KeyCode.LEFT);
 
-        input.addAction(new UserAction("Rotate Right") {
-            @Override
-            protected void onAction() {
-                player.setRotation(90.0);
-                onShoot();
-            }
-        }, KeyCode.RIGHT);
-
-        input.addAction(new UserAction("Rotate Up") {
-            @Override
-            protected void onAction() {
-                player.setRotation(0.0);
-                onShoot();
-            }
-        }, KeyCode.UP);
-
-        input.addAction(new UserAction("Rotate Down") {
-            @Override
-            protected void onAction() {
-                player.setRotation(180.0);
-                onShoot();
-            }
-        }, KeyCode.DOWN);
-        */
 }
+
+/*
+input.addAction(new UserAction("Rotate Left") {
+        player.setRotation(-90.0);
+        onShoot();
+input.addAction(new UserAction("Rotate Right") {
+        player.setRotation(90.0);
+        onShoot();
+input.addAction(new UserAction("Rotate Up") {
+        player.setRotation(0.0);
+        onShoot();
+input.addAction(new UserAction("Rotate Down") {
+        player.setRotation(180.0);
+        onShoot();
+*/
