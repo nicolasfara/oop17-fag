051e3ad0b0c0f7dfcd9fb9385257e29e7c72241c
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-08-23 18:55:21 +0200

    create entity type

--- src/main/java/it/unibo/goffo/fag/entities/Player.java
+++ src/main/java/it/unibo/goffo/fag/entities/Player.java
@@ -1,8 +1,70 @@
 package it.unibo.goffo.fag.entities;
 
+import it.unibo.goffo.fag.movement.EntityMovement;
+import it.unibo.goffo.fag.movement.MoveDirection;
+
 /**
  * The {@code Character} controlled by the user
  */
-public class Player extends Character{
+public class Player extends Character {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void attack() {
 
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void playWalkAnimation(final MoveDirection direction) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void playIdleAnimation(final MoveDirection direction) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void decrementLife(final int damage) {
+
+    }
+
+    /**
+     * Move the entity up.
+     */
+    public void moveUp() {
+        getComponent(EntityMovement.class).moveUp();
+    }
+
+    /**
+     * Move the entity down.
+     */
+    public void moveDown() {
+        getComponent(EntityMovement.class).moveDown();
+    }
+
+    /**
+     * Move the entity left.
+     */
+    public void moveLeft() {
+        getComponent(EntityMovement.class).moveLeft();
+    }
+
+    /**
+     * Move the entity right.
+     */
+    public void moveRight() {
+        getComponent(EntityMovement.class).moveRight();
+    }
 }
+
--- src/main/java/it/unibo/goffo/fag/entities/Zombie.java
+++ src/main/java/it/unibo/goffo/fag/entities/Zombie.java
@@ -1,7 +1,40 @@
 package it.unibo.goffo.fag.entities;
 
+import it.unibo.goffo.fag.movement.MoveDirection;
+
 /**
  * Defines all enemy {@code Character}s controlled by the game.
  */
-public class Zombie extends Character{
+public class Zombie extends Character {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void attack() {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void playWalkAnimation(final MoveDirection direction) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void playIdleAnimation(final MoveDirection direction) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void decrementLife(final int damage) {
+
+    }
 }
