342fe0f4dc47f382c9c3a092fb5c67bb2172e54f
Leonardo Marini <leonardo.marini3@studio.unibo.it>
2018-08-25 21:48:17 +0200

    update player's creation

--- src/main/java/it/unibo/goffo/fag/entities/builders/SimpleFactory.java
+++ src/main/java/it/unibo/goffo/fag/entities/builders/SimpleFactory.java
@@ -1,16 +1,24 @@
 package it.unibo.goffo.fag.entities.builders;
 
 import com.almasb.fxgl.app.FXGL;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
 import it.unibo.goffo.fag.entities.FagType;
 import it.unibo.goffo.fag.entities.Player;
 import it.unibo.goffo.fag.entities.Zombie;
+import it.unibo.goffo.fag.movement.EntityMovement;
 
 public class SimpleFactory {
 
     Player player = (Player) FagEntities.builder(Player.class)
-            .at(100, 100)
             .type(FagType.PLAYER)
-            .buildAndAttach(FXGL.getApp().getGameWorld());
+            .from(data)
+            .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+            .with(new CollidableComponent(true))
+            .with(new EntityMovement(1))
+            .build();
+
 
     Zombie zombie = (Zombie) FagEntities.builder(Zombie.class)
             .at(200, 200)
