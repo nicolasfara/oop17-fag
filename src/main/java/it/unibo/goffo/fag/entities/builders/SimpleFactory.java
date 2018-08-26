d48e600fbe344e8bd4410789782aa712a5cb3252
Leonardo Marini <leonardo.marini3@studio.unibo.it>
2018-08-26 16:56:45 +0200

    spawn random on map

--- src/main/java/it/unibo/goffo/fag/entities/builders/SimpleFactory.java
+++ src/main/java/it/unibo/goffo/fag/entities/builders/SimpleFactory.java
@@ -8,21 +8,36 @@ import it.unibo.goffo.fag.entities.FagType;
 import it.unibo.goffo.fag.entities.Player;
 import it.unibo.goffo.fag.entities.Zombie;
 import it.unibo.goffo.fag.movement.EntityMovement;
+import javafx.geometry.Point2D;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SimpleFactory {
 
     Player player = (Player) FagEntities.builder(Player.class)
             .type(FagType.PLAYER)
-            .from(data)
-            .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+            .at(50, 50)//.from(data)
+            // data.<Integer>get("width"), data.<Integer>get("height")
+            .bbox(new HitBox(BoundingShape.box(64, 64)))
             .with(new CollidableComponent(true))
             .with(new EntityMovement(1))
             .build();
 
 
     Zombie zombie = (Zombie) FagEntities.builder(Zombie.class)
-            .at(200, 200)
+            .at(randomPoint(/*max height, max width*/))
             .type(FagType.SIMPLE_ZOMBIE)
             .buildAndAttach(FXGL.getApp().getGameWorld());
 
+    private static Point2D randomPoint(){
+        final Point2D position;
+        double x, y;
+
+        x = ThreadLocalRandom.current().nextDouble();
+        y = ThreadLocalRandom.current().nextDouble();
+        position = new Point2D(x, y);
+
+        return position;
+    }
 }
+
