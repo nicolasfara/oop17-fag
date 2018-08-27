4ac02222ac1f21473bf0298751920bd7c38705f1
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-08-27 21:35:55 +0200

    general refactor

--- src/main/java/it/unibo/goffo/fag/entities/builders/ZombieFactory.java
+++ src/main/java/it/unibo/goffo/fag/entities/builders/ZombieFactory.java
@@ -7,49 +7,43 @@ import javafx.geometry.Point2D;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-public class ZombieFactory {
+/**
+ *
+ */
+public final class ZombieFactory {
 
-//    Player player = (Player) FagEntities.builder(Player.class)
-//            .type(FagType.PLAYER)
-//            .at(50, 50)//.from(data)
-//            // data.<Integer>get("width"), data.<Integer>get("height")
-//            .bbox(new HitBox(BoundingShape.box(64, 64)))
-//            .with(new CollidableComponent(true))
-//            .with(new EntityMovement(1))
-//            .build();
+    private ZombieFactory() { }
 
-
-    private ZombieFactory() {}
-
+    /**
+     *
+     * @return
+     */
     public static Zombie createSimpleZombie() {
-        final Zombie zombie = (Zombie) FagEntities.builder(Zombie.class)
-                .at(randomPoint(/*max height, max width*/))
+        return (Zombie) FagEntities.builder(Zombie.class)
+                .at(randomPoint())
                 .type(FagType.SIMPLE_ZOMBIE)
                 .with(new ZombieAnimationImpl())
                 .build();
-
-        return zombie;
     }
 
+    /**
+     *
+     * @return
+     */
     public static Zombie createAdvancedZombie() {
-        final Zombie zombie = (Zombie) FagEntities.builder(Zombie.class)
-                .at(randomPoint(/*max height, max width*/))
+        return (Zombie) FagEntities.builder(Zombie.class)
+                .at(randomPoint())
                 .type(FagType.ADVANCE_ZOMBIE)
                 .with(new ZombieAnimationImpl())
                 .build();
-
-        return zombie;
     }
 
-    private static Point2D randomPoint(){
-        final Point2D position;
-        double x, y;
-
-        x = ThreadLocalRandom.current().nextDouble();
-        y = ThreadLocalRandom.current().nextDouble();
-        position = new Point2D(x, y);
-
-        return position;
+    /**
+     *
+     * @return
+     */
+    private static Point2D randomPoint() {
+        return new Point2D(ThreadLocalRandom.current().nextDouble(), ThreadLocalRandom.current().nextDouble());
     }
 }
 
