99213f0cdc0b6bac190ff9363e1c8af4e3d4869d
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-08-27 09:27:43 +0200

    view spawn create

--- src/main/java/it/unibo/goffo/fag/spawn/view/SpawnViewImpl.java
+++ src/main/java/it/unibo/goffo/fag/spawn/view/SpawnViewImpl.java
@@ -1,4 +1,33 @@
 package it.unibo.goffo.fag.spawn.view;
 
-public class SpawnViewImpl {
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import it.unibo.goffo.fag.entities.Character;
+import it.unibo.goffo.fag.spawn.controller.SpawnController;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Concrete class used to show the entity into the game world.
+ */
+public class SpawnViewImpl implements SpawnView {
+
+    private final SpawnController spawnController;
+
+    /**
+     * Default constructor use to take a reference of the controller.
+     * @param spawnController the spawn controller.
+     */
+    public SpawnViewImpl(final SpawnController spawnController) {
+        this.spawnController = spawnController;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void subscribeHandler(final Consumer<Character> consumer) {
+        Disposable disposable = spawnController.getObservable().subscribe(consumer);
+        final CompositeDisposable compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(disposable);
+    }
 }
