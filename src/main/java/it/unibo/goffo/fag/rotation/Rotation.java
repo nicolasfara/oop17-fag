9073c4e87808fefc232481c75689d8e64c5381ec
andrcas <andrea.casadei22@studio.unibo.it>
2018-08-28 02:10:27 +0200

    fix rotation class

--- src/main/java/it/unibo/goffo/fag/rotation/Rotation.java
+++ src/main/java/it/unibo/goffo/fag/rotation/Rotation.java
@@ -1,4 +1,26 @@
 package it.unibo.goffo.fag.rotation;
 
+/**
+ * Interface used to generate a rotation assignable to an entity.
+ */
 public interface Rotation {
+    /**
+     * The method rotate the entity face up.
+     */
+    void rotateUp();
+
+    /**
+     * The method rotate the entity face down.
+     */
+    void rotateDown();
+
+    /**
+     * The method rotate the entity face left.
+     */
+    void rotateLeft();
+
+    /**
+     * The method rotate the entity face right.
+     */
+    void rotateRight();
 }
