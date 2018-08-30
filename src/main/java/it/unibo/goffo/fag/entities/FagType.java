771888fd96a3d0b0035eea8029caacad803eb7b3
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-08-30 12:21:17 +0200

    removed unused semicolon

--- src/main/java/it/unibo/goffo/fag/entities/FagType.java
+++ src/main/java/it/unibo/goffo/fag/entities/FagType.java
@@ -4,5 +4,5 @@ package it.unibo.goffo.fag.entities;
  * Enum that model all type of entities in the game.
  */
 public enum FagType {
-    PLAYER, SIMPLE_ZOMBIE, ADVANCE_ZOMBIE;
+    PLAYER, SIMPLE_ZOMBIE, ADVANCE_ZOMBIE
 }
