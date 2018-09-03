6b7a0ec35d0a542aaf035f421492ac76919659cf
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-03 19:55:17 +0200

    menu - new gameapplication class to test menus

--- src/main/java/it/unibo/goffo/fag/ui/menu/MenuApplication.java
+++ src/main/java/it/unibo/goffo/fag/ui/menu/MenuApplication.java
@@ -1,4 +1,40 @@
 package it.unibo.goffo.fag.ui.menu;
 
-public class MenuApplication {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class MenuApplication extends GameApplication {
+    /**
+     * Initialize app settings.
+     *
+     * @param settings app settings
+     */
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("Fight Avenge Guerrilla");
+        settings.setMenuEnabled(true);
+        settings.setSceneFactory(new FAGMenuFactory());
+    }
+
+        @Override
+    protected void initUI() {
+        Rectangle bg = new Rectangle(600, 600);
+        bg.setFill(Color.BLUE);
+        getGameScene().addUINode(bg);
+    }
+
+    public static void exitGame() {
+        new MenuApplication().exit();
+    }
+
+    /**
+     * Main.
+     * @param args Args.
+     */
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
 }
