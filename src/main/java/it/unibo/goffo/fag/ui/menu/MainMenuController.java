83a4e068b112e69075f7269cecd57f6575a250ec
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-03 19:54:08 +0200

    menu - edit fxml controller class - start and exit game

--- src/main/java/it/unibo/goffo/fag/ui/menu/MainMenuController.java
+++ src/main/java/it/unibo/goffo/fag/ui/menu/MainMenuController.java
@@ -1,15 +1,17 @@
 package it.unibo.goffo.fag.ui.menu;
 
 import com.almasb.fxgl.app.FXGL;
-import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.ui.UIController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
+import javafx.scene.text.Text;
+
 
 public class MainMenuController implements UIController {
+
+    FAGMenu mainMenu;
 
     @FXML
     private Button btnStartGame;
@@ -20,27 +22,37 @@ public class MainMenuController implements UIController {
     @FXML Button btnExit;
 
     @FXML
+    private Text menuTitle;
+
+    public void setMenuTitle(final String text) {
+        this.menuTitle.setText(text);
+    }
+
+    public Text getMenuTitle() {
+        return this.menuTitle;
+    }
+
+    @FXML
     private void startGame() {
+/*        mainMenu = newImpl FAGMenu(FXGL.getApp(), MenuType.MAIN_MENU);
+        this.mainMenu.newGame();*/
+        FXGL.getApp().getMenuListener().onNewGame();
     }
 
     @FXML
     private void showScores() {
-
     }
 
     @FXML
     private void onExit() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Exit?");
+/*        MenuApplication menuApplication = newImpl MenuApplication();
+        Alert alert = newImpl Alert(Alert.AlertType.CONFIRMATION, "Exit?");
 
         alert.showAndWait()
                 .filter(response -> response == ButtonType.OK)
-                .ifPresent(response -> Platform.exit());
+                .ifPresent(response -> menuApplication.exitGame());*/
 
-/*        FXGL.getApp().getDisplay().showConfirmationBox("Exit?", yes -> {
-            if (yes) {
-                Platform.exit();
-            }
-        });*/
+        FXGL.getApp().getMenuListener().onExit();
     }
 
     @Override
