e944bd862d5bd8c78b84c3f1ff9dcd288836513d
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-03 19:52:46 +0200

    menu - add scenefactory class

--- src/main/java/it/unibo/goffo/fag/ui/menu/FAGMenuFactory.java
+++ src/main/java/it/unibo/goffo/fag/ui/menu/FAGMenuFactory.java
@@ -1,4 +1,16 @@
 package it.unibo.goffo.fag.ui.menu;
 
-public class FAGMenuFactory {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.scene.FXGLMenu;
+import com.almasb.fxgl.scene.SceneFactory;
+import com.almasb.fxgl.scene.menu.MenuType;
+import org.jetbrains.annotations.NotNull;
+
+public class FAGMenuFactory extends SceneFactory {
+
+    @NotNull
+    @Override
+    public FXGLMenu newMainMenu(final GameApplication app) {
+        return new FAGMenu(app, MenuType.MAIN_MENU);
+    }
 }
