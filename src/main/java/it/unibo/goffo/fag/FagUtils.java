412e807cd35b3651f700f3998f0e7783a3d873d3
nicolas-farabegoli <nicolas.farabegoli@studio.unibo.it>
2018-09-04 09:21:26 +0200

    general refactor

--- src/main/java/it/unibo/goffo/fag/FagUtils.java
+++ src/main/java/it/unibo/goffo/fag/FagUtils.java
@@ -4,16 +4,55 @@ package it.unibo.goffo.fag;
  * Utility class.
  */
 public final class FagUtils {
+    /**
+     * Tile size, used for fit the level map.
+     */
     public static final int TILE_SIZE = 64;
-    public static final String APPLICATION_NAME = "Final Avenge Guerrilla";
+
+    /**
+     * Block size for AI grid.
+     */
     public static final int BLOCK_SIZE = 40;
+
+    /**
+     * X size for zombie entity.
+     */
     public static final int ZOMBIE_SIZE_X = 20;
+
+    /**
+     * Y size for zombie entity.
+     */
     public static final int ZOMBIE_SIZE_Y = 20;
+
+    /**
+     * X size for player entity.
+     */
     public static final int PLAYER_SIZE_X = 20;
+
+    /**
+     * Y size for player entity.
+     */
     public static final int PLAYER_SIZE_Y = 20;
-    public static final int MAP_SIZE = 600;
-    public static final int WIDTH_SCREEN = MAP_SIZE;
-    public static final int HEIGHT_SCREEN = MAP_SIZE;
+
+    /**
+     * Map size for AI.
+     */
+    static final int MAP_SIZE = 600;
+
+    /**
+     * Width screen.
+     */
+    static final int WIDTH_SCREEN = MAP_SIZE;
+
+    /**
+     * Application name.
+     */
+    static final String APPLICATION_NAME = "Final Avenge Guerrilla";
+
+    /**
+     * Height screen.
+     */
+    static final int HEIGHT_SCREEN = MAP_SIZE;
 
     private FagUtils() { }
 }
--- src/main/java/it/unibo/goffo/fag/LevelFactory.java
+++ src/main/java/it/unibo/goffo/fag/LevelFactory.java
@@ -1,6 +1,10 @@
 package it.unibo.goffo.fag;
 
-import com.almasb.fxgl.entity.*;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
@@ -9,7 +13,7 @@ import com.almasb.fxgl.physics.PhysicsComponent;
 public class LevelFactory implements EntityFactory {
 
     @Spawns("collidable")
-    public Entity newCollidable(SpawnData data) {
+    public Entity newCollidable(final SpawnData data) {
         return Entities.builder()
                 .from(data)
                 .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
