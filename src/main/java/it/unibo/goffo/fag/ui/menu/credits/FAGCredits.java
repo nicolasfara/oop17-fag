07869106a2bc189370bf0591d39dbd3a3a5cd018
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-04 13:04:28 +0200

    menu - add credits class

---
+++ src/main/java/it/unibo/goffo/fag/ui/menu/credits/FAGCredits.java
@@ -0,0 +1,25 @@
+package it.unibo.goffo.fag.ui.menu.credits;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class FAGCredits {
+    FAGCredits() {
+
+        /*
+        no, usare FXML
+         */
+
+
+/*        List<String> list = new ArrayList<>();
+        list.add("Sample: A sample credit");
+        list.add("-----------------------");
+
+        IntStream.range(0, 50).forEach(i -> list.add("Line number: " + i));
+
+        list.add("Do not forget to split long credits");
+        list.add("so that the flow integrity is");
+        list.add("maintained!");*/
+    }
+}
