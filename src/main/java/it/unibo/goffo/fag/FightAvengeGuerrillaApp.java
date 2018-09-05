dd6beaa1a246c197121104e17f1ec3495c240a6e
andrcas <andrea.casadei22@studio.unibo.it>
2018-09-05 12:16:44 +0200

    add key control for rotation

--- src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaApp.java
+++ src/main/java/it/unibo/goffo/fag/FightAvengeGuerrillaApp.java
@@ -1,16 +1,43 @@
 package it.unibo.goffo.fag;
 
+import com.almasb.fxgl.app.FXGL;
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.extra.ai.pathfinding.AStarGrid;
+import com.almasb.fxgl.parser.tiled.TiledMap;
 import com.almasb.fxgl.settings.GameSettings;
+import com.almasb.fxgl.ui.UI;
+import it.unibo.goffo.fag.animation.PlayerAnimationImpl;
+import it.unibo.goffo.fag.entities.FagType;
+import it.unibo.goffo.fag.entities.Player;
+import it.unibo.goffo.fag.entities.builders.FagEntities;
+import it.unibo.goffo.fag.movement.EntityMovement;
+import it.unibo.goffo.fag.movement.MoveDirection;
+import it.unibo.goffo.fag.spawn.controller.SpawnControllerImpl;
+import it.unibo.goffo.fag.spawn.view.SpawnView;
+import it.unibo.goffo.fag.spawn.view.SpawnViewImpl;
+import it.unibo.goffo.fag.life.controller.LifeController;
+import it.unibo.goffo.fag.life.controller.LifeControllerImpl;
+import it.unibo.goffo.fag.ui.hud.HUDController;
+
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.application.Platform;
+import javafx.scene.input.KeyCode;
+
+import static it.unibo.goffo.fag.FagUtils.APPLICATION_NAME;
+import static it.unibo.goffo.fag.FagUtils.HEIGHT_SCREEN;
+import static it.unibo.goffo.fag.FagUtils.WIDTH_SCREEN;
+import static it.unibo.goffo.fag.FagUtils.MAP_SIZE;
 
 /**
  * Main class, used to launch FXGL.
  */
 public class FightAvengeGuerrillaApp extends GameApplication {
 
-    private static final int WIDTH_SCREEN = 800;
-    private static final int HEIGHT_SCREEN = 600;
-    private static final String APPLICATION_NAME = "Final Avenge Guerrilla";
+    private AStarGrid grid;
+    private Player player;
+    private LifeController lifeController;
 
     /**
      * Main method launch the game engine.
@@ -21,6 +48,22 @@ public class FightAvengeGuerrillaApp extends GameApplication {
     }
 
     /**
+     * Return the grid used in the game.
+     * @return the game's grid.
+     */
+    public AStarGrid getGrid() {
+        return this.grid;
+    }
+
+    /**
+     * Method that return the player in the game.
+     * @return Return the player.
+     */
+    public Entity getPlayer() {
+        return getGameWorld().getSingleton(FagType.PLAYER).get();
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
@@ -28,6 +71,7 @@ public class FightAvengeGuerrillaApp extends GameApplication {
         settings.setWidth(WIDTH_SCREEN);
         settings.setHeight(HEIGHT_SCREEN);
         settings.setTitle(APPLICATION_NAME);
+        this.lifeController = new LifeControllerImpl();
     }
 
     /**
@@ -35,7 +79,104 @@ public class FightAvengeGuerrillaApp extends GameApplication {
      */
     @Override
     protected void initInput() {
-        super.initInput();
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onActionBegin() {
+                player.playWalkAnimation(MoveDirection.RIGHT);
+            }
+
+            @Override
+            protected void onAction() {
+                player.moveRight();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                player.playIdleAnimation(MoveDirection.RIGHT);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onActionBegin() {
+                player.playWalkAnimation(MoveDirection.LEFT);
+            }
+
+            @Override
+            protected void onAction() {
+                player.moveLeft();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                player.playIdleAnimation(MoveDirection.LEFT);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onActionBegin() {
+                player.playWalkAnimation(MoveDirection.UP);
+            }
+
+            @Override
+            protected void onAction() {
+                player.moveUp();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                player.playIdleAnimation(MoveDirection.UP);
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onActionBegin() {
+                player.playWalkAnimation(MoveDirection.DOWN);
+            }
+
+            @Override
+            protected void onAction() {
+                player.moveDown();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                player.playIdleAnimation(MoveDirection.DOWN);
+            }
+        }, KeyCode.S);
+
+        input.addAction(new UserAction("Rotate Left") {
+            @Override
+            protected void onAction() {
+                player.rotateLeft();
+            }
+        }, KeyCode.LEFT);
+
+        input.addAction(new UserAction("Rotate Right") {
+            @Override
+            protected void onAction() {
+                player.rotateRight();
+            }
+        }, KeyCode.RIGHT);
+
+        input.addAction(new UserAction("Rotate Up") {
+            @Override
+            protected void onAction() {
+                player.rotateUp();
+            }
+        }, KeyCode.UP);
+
+        input.addAction(new UserAction("Rotate Down") {
+            @Override
+            protected void onAction() {
+                player.rotateDown();
+            }
+        }, KeyCode.DOWN);
+
     }
 
     /**
@@ -43,14 +184,50 @@ public class FightAvengeGuerrillaApp extends GameApplication {
      */
     @Override
     protected void initGame() {
-        super.initGame();
+        grid = new AStarGrid(MAP_SIZE, MAP_SIZE);
+        SpawnView spawnView = new SpawnViewImpl(SpawnControllerImpl.getInstance());
+        spawnView.subscribeHandler(e -> Platform.runLater(() -> FXGL.getApp().getGameWorld().addEntity(e)));
+
+        getGameWorld().addEntityFactory(new LevelFactory());
+        TiledMap map = getAssetLoader().loadJSON("level0.json", TiledMap.class);
+        getGameWorld().setLevelFromMap(map);
+
+        player = (Player) FagEntities.builder(Player.class)
+                .type(FagType.PLAYER)
+                .at(200, 200)
+                .with(new EntityMovement(1))
+                .with(new PlayerAnimationImpl())
+                .buildAndAttach(getGameWorld());
+        /*lifeController.bindLife();*/
+        this.getGameState().setValue("playerLife", 1.0);
+    }
+
+    /**
+     * {@inheritDoc}Math.abs
+     */
+    @Override
+    protected void initPhysics() {
+        super.initPhysics();
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    protected void initPhysics() {
-        super.initPhysics();
+    protected void initUI() {
+        /*
+         * No need to use FXMLLoader. FXGL's AssetLoader does the job.
+         * Remember to not use 'fx:controller' attribute in your fxml file.
+         */
+        final HUDController hudController = new HUDController(getGameScene(), getGameState());
+        final UI hud = getAssetLoader().loadUI("hud.fxml", hudController);
+
+        /* NOT WORKING
+        hudController.getPlayerLife().progressProperty().bind(this.absLifeController.getLifeProperty());
+        */
+
+        hudController.getPlayerLife().progressProperty().bind(
+                this.getGameState().doubleProperty("playerLife"));
+        getGameScene().addUI(hud);
     }
 }
