74e5811eb7649f4b6d5a7b7439062cc885b3d18e
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-08 12:20:04 +0200

    menu - use static variables from config file

---
+++ src/main/java/it/unibo/goffo/fag/FAGConfig.java
@@ -0,0 +1,11 @@
+package it.unibo.goffo.fag;
+
+public class FAGConfig {
+
+    public static final int DISP_WIDTH = 800;
+    public static final int DISP_HEIGHT = 800;
+
+    public int getDispWidth() {
+        return DISP_WIDTH;
+    }
+}
