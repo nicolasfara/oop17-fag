b5fd4e61d5bb38616ca6f3dae239ae9ae818e54c
fabriziomargotta <fabrizio.margotta@studio.unibo.it>
2018-09-17 21:03:00 +0200

    game - fix points

--- src/main/java/it/unibo/goffo/fag/collision/BulletZombieCollision.java
+++ src/main/java/it/unibo/goffo/fag/collision/BulletZombieCollision.java
@@ -28,7 +28,7 @@ public class BulletZombieCollision implements Collision<Bullet, Zombie> {
         } catch (CharacterDiesException e1) {
             z1.removeFromWorld();
             FXGL.getApp().getGameState().setValue("points",
-                    String.valueOf(Integer.valueOf(FXGL.getApp().getGameState().getString("round")) + 1));
+                    String.valueOf(Integer.valueOf(FXGL.getApp().getGameState().getString("points")) + 1));
         }
     }
 }
